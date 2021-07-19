b78fe74fd67489e245ceeab3e1ad8b08434fb267
Jay Babel <jaybabel@gmail.com>
2021-07-19 13:13:47 -0500

    Removed kbhit() - not used

--- j8cm.c
+++ j8cm.c
@@ -60,15 +60,6 @@ int bit,
 	     tcsetattr(0, TCSANOW, &new_termios);
 	 }
 
-	 int kbhit()
-	 {
-	     struct timeval tv = { 0L, 0L };
-	     fd_set fds;
-	     FD_ZERO(&fds);
-	     FD_SET(0, &fds);
-	     return select(1, &fds, NULL, NULL, &tv);
-	 }
-
 	 int getch()
 	 {
 	     int r;
